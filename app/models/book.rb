2cc06cd9d581a28a6fbf87564befdf50c0cbda4c
Sam Huhn <smhuhn05@gmail.com>
2026-02-02 20:17:28 -0600

    added user functionality

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,6 @@
 class Book < ApplicationRecord
-  validates :title, presence: true
-  validates :author, presence: true
-  validates :price, presence: true,
-                    numericality: { greater_than_or_equal_to: 0 }
-  validates :published_date, presence: true
+  has_many :users, through: :user_books
+  has_many :user_books
+
+  validates :title, :author, :price,:published_date, presence: true
 end
